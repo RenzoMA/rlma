b4ab732dc93987141af9d847323b69b9588cb11b
Renzo Maldonado <renzoma89@gmail.com>
2023-07-05 01:08:13 -0500

    fix typing

--- libs/trivia/app-config/src/lib/config-data.ts
+++ libs/trivia/app-config/src/lib/config-data.ts
@@ -1,15 +1,16 @@
 import { InjectionToken, APP_INITIALIZER } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
+import { AppConfig } from './models';
 
-export const CONFIG_DATA = new InjectionToken<any>('configData');
-export const CONFIG = { data: {} };
+export const CONFIG_DATA = new InjectionToken<{ data: AppConfig }>(
+  'configData'
+);
+export const CONFIG: { data: AppConfig } = { data: { apiEndpoint: '' } };
 
-export function appInitializer(
-  http: HttpClient
-): () => Observable<any> {
+export function appInitializer(http: HttpClient): () => Observable<any> {
   return () => {
-    return http.get('./assets/config.json').pipe(
+    return http.get<AppConfig>('./assets/config.json').pipe(
       tap((config) => {
         CONFIG.data = config;
       })
